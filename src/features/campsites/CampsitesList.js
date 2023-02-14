9cf059c07deb2050d100df570ce3879df9207a03
aliersh <ariel.diaz.borquez@icloud.com>
2023-02-14 17:23:58 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -6,7 +6,7 @@ const CampsitesList = () => {
     const campsites = selectAllCampsites();
     return (
         <Row className="ms-auto">
-            {campsites.map(campsite => {
+            {campsites.map((campsite) => {
                 return (
                 <Col md='5' className="m-4" key={campsite.id}>
                     <CampsiteCard campsite={campsite} />
